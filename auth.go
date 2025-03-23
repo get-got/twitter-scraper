979091324bcc6878ed530c51d101a39c79c6fef4
Drn <43052079+get-got@users.noreply.github.com>
2025-03-23 12:59:50 -0700

    x2

--- auth.go
+++ auth.go
@@ -89,7 +89,7 @@ func (s *Scraper) getFlow(data map[string]interface{}) (*flow, error) {
 	headers := http.Header{
 		"Authorization":             []string{"Bearer " + s.bearerToken},
 		"Content-Type":              []string{"application/json"},
-		"User-Agent":                []string{s.userAgent},
+		"User-Agent":                []string{"TwitterAndroid/99"},
 		"X-Guest-Token":             []string{s.guestToken},
 		"X-Twitter-Auth-Type":       []string{"OAuth2Client"},
 		"X-Twitter-Active-User":     []string{"yes"},
